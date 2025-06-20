686a0e5dd6a59fc7d91aeba449118d77b302322d
mahesh-0912 <m9873272@gmail.com>
2025-06-20 17:52:18 +0530

    change in package

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
 });
